968d3a45b7417c4ff1358043c9acf1b86ec2fa2f
aaa928 <dydwn0928@gmail.com>
2020-10-06 04:40:42 -0400

    2-factor issue clear

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "18 it works\n" 
+    return "2-factor issue, but now it works\n" 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
